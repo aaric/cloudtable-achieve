3f89e039c222276fab8b7a46f1d05635c942a7cd
qlhan <qlhan@incarcloud.com>
2020-05-18 11:32:45 +0800

    0.0.1-SNAPSHOT init

---
+++ cloudtable-client/src/test/java/com/sample/cloudtable/CloudTableTests.java
@@ -0,0 +1,19 @@
+package com.sample.cloudtable;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * CloudTableTests
+ *
+ * @author Aaric, created on 2020-05-18T11:12.
+ * @version 0.0.1-SNAPSHOT
+ */
+@Slf4j
+public class CloudTableTests {
+
+    @Test
+    public void testSayHello() {
+        log.info("hello world");
+    }
+}
